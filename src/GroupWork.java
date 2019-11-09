715e756c00b955fa5f39457563973fa9ff5c39db
SabaMicheal <57517096+SabaMicheal@users.noreply.github.com>
2019-11-09 17:56:23 -0600

    update

---
+++ src/GroupWork.java
@@ -0,0 +1,14 @@
+public class GroupWork {
+
+    public static void main(String[] args) {
+      
+            int [] arr2 = {-1,3,-4,-2,32,2,-23,1};
+            // printNegativeNumbers(arr2);
+            //  changeNegativeNumbers(arr2,100);
+
+
+        }
+
+
+    }
+
